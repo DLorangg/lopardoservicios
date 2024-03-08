878b0115cf70aa708888dacf7de1aa45899139b6
DLorangg <lorang.damian.epet20@gmail.com>
2024-03-08 16:28:31 -0300

    Agregando boton para cerrar sesion

--- frontend/src/Components/Crud/Crud.jsx
+++ frontend/src/Components/Crud/Crud.jsx
@@ -1,47 +1,16 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './Crud.css'
+import React from 'react';
 
-function Crud() {
-  const [data, setData] = useState([]);
-  
-  useEffect(() => {
-    axios.get('http://localhost:8081/')
-      .then(res => {
-        console.log(res.data);
-        setData(res.data);
-      })
-      .catch(err => console.log(err));
-  }, []);
+function Crud({ onLogout }) {
+  const handleLogout = () => {
+    localStorage.setItem('loggedIn', 'false');
+    onLogout();
+  };
+
   return (
     <div>
-      <div>
-        <h2>Datos</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((dato, index) => (
-              <tr key={index}>
-                <td>{index}</td>
-                <td>{dato.datitos}</td>
-                <td>
-                  <button>Editar</button>
-                  <button>Borrar</button>
-                  <button></button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <button onClick={handleLogout}>Cerrar sesión</button>
     </div>
   );
 }
 
-export default Crud
+export default Crud;
--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import {useState } from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
 import LogoCompleto from '../../Assets/Logo_Completo.png';
