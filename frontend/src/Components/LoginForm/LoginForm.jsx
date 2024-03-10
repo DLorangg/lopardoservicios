50d289d67d0ecd51421cb3ebc2b9fa24342824c1
DLorangg <lorang.damian.epet20@gmail.com>
2024-03-10 14:41:10 -0300

    Agregando toast

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -1,14 +1,15 @@
-import {useState } from 'react';
+import React, { useState } from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
 import LogoCompleto from '../../Assets/Logo_Completo.png';
 import axios from 'axios';
 import { Navigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginForm = ({ setLoggedIn }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [loginError, setLoginError] = useState(false);
 
     const handleLogin = () => {
         axios.post('http://localhost:8081/login', {username, password})
@@ -18,7 +19,7 @@ const LoginForm = ({ setLoggedIn }) => {
         })
         .catch(err => {
             console.log(err);
-            setLoginError(true);
+            toast.error('Error de inicio de sesión. Por favor, inténtelo de nuevo.');
         });
     };
 
@@ -27,30 +28,31 @@ const LoginForm = ({ setLoggedIn }) => {
     }
 
     return (
-        <div className='wrapper'>
-            <form onSubmit={e => {
-                e.preventDefault();
-                handleLogin();
-            }}>
-                <img src={LogoCompleto} alt="Logo" /> 
-                <h1>Iniciar sesión</h1>
+        <div>
+            <ToastContainer/> 
+            <div className='wrapper'>
+                <form onSubmit={e => {
+                    e.preventDefault();
+                    handleLogin();
+                }}>
+                    <img src={LogoCompleto} alt="Logo" /> 
+                    <h1>Iniciar sesión</h1>
 
-                {loginError && <p>Error de inicio de sesión. Por favor, inténtelo de nuevo.</p>}
+                    <div className="input-box">
+                        <input type="text" name="username" id="username" placeholder="Nombre de usuario" required
+                        onChange={e => setUsername(e.target.value)}/>
+                        <FaUser className='icon'/>            
+                    </div>
 
-                <div className="input-box">
-                    <input type="text" name="username" id="username" placeholder="Nombre de usuario" required
-                    onChange={e => setUsername(e.target.value)}/>
-                    <FaUser className='icon'/>            
-                </div>
+                    <div className="input-box">
+                        <input type="password" name="password" id="password" placeholder="Contraseña" required
+                        onChange={e => setPassword(e.target.value)}/>
+                        <FaLock className='icon'/>            
+                    </div>
 
-                <div className="input-box">
-                    <input type="password" name="password" id="password" placeholder="Contraseña" required
-                    onChange={e => setPassword(e.target.value)}/>
-                    <FaLock className='icon'/>            
-                </div>
-
-                <button type="submit">Iniciar sesión</button>
-            </form>
+                    <button type="submit">Iniciar sesión</button>
+                </form>
+            </div>
         </div>
     );
 }
